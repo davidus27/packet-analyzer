e2f8e54cc8281463555163466c91a6aacc3b913d
davidus27 <davidisbest27@gmail.com>
2020-09-07 00:31:24 +0200

    adding constants

---
+++ src/constants.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace Ethernet
+{
+    const uint8_t MAC_SIZE = 6; // bytes
+    const uint8_t ETHER_TYPE = 2; // bytes
+    const uint8_t ETHER_TYPE_OFFSET = 12; // bytes
+    const uint8_t IPX = 3; // bytes
+    const uint8_t FCS = 4; // bytes
+}
+
+namespace IP
+{
+
+}
+
+namespace UDP
+{
+
+}
+
+
+// etc...
