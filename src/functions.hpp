a155d0fcc5686e55576865c652da4c61586e1aeb
davidus27 <davidisbest27@gmail.com>
2020-09-07 00:43:55 +0200

    declared ProcessedPacket class for saving info about each packet and creating enum classes for future work

--- src/functions.hpp
+++ src/functions.hpp
@@ -1,5 +1,46 @@
 #pragma once
 
+
+namespace frame
+{
+        
+    enum class EthernetStandard
+    {
+        EthernetII,
+        NovellRAW,
+        IEE,
+        IEE_LLC_SNAP
+    };
+
+    enum class Layer2
+    {
+
+    };
+
+    enum class Layer3
+    {
+
+    };
+
+    class ProcessedPacket
+    {
+    private:
+        const uint32_t packet_size_recv;
+        const uint32_t packet_size_real;
+        EthernetStandard eth_type;
+        const uint8_t* mac_dst;
+        const uint8_t* mac_src;
+        uint8_t* ip_dst;
+        uint8_t* ip_src;
+        Layer2 protocol;
+        Layer3 p2;
+        
+    public:
+        ProcessedPacket(const struct pcap_pkthdr* packet_header, const uint8_t* packet_body);
+        ~ProcessedPacket();
+    };
+}
+
 void process_packet(
     uint8_t* args,
     const struct pcap_pkthdr* packet_header,
