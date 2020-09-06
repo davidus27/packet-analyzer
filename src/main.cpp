3893f0259a0bfa59555a0040117cb2cb8f9cec23
davidus27 <davidisbest27@gmail.com>
2020-09-07 00:45:36 +0200

    removing constants and structuringa and loading path from argument

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,30 @@
 #include <pcap.h>
 #include "functions.hpp"
 
-// Constants
-const char* file_path = "../../vzorky_pcap_na_analyzu/vzorky_pcap_na_analyzu/eth-1.pcap";
-constexpr short timeout = 1000; //ms
-constexpr short packets_amount = 0; // unlimited
-
-
 int main(int argc, char *argv[])
 {
+
     char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t* handle = pcap_open_offline(file_path, errbuf);
     const uint8_t* packet;
     struct pcap_pkthdr packet_header;
-    int timeout_limit = 10000;
-    
-
-    pcap_loop(handle, 0, process_packet, nullptr);
-
-    if(handle)
+    if(argc > 1)
     {
-        pcap_close(handle);
-        std::cout<<"File: "<< file_path<<" closed\n";
+        pcap_t* handle = pcap_open_offline(argv[1], errbuf);
+        if(handle)
+        {
+            pcap_loop(handle, 0, process_packet, nullptr);
+            pcap_close(handle);
+            std::cout<<"File: "<< argv[1]<<" closed\n";
+        }
+        else
+        {
+            std::cout << "File was not found.\n";
+        }
     }
+    else
+    {
+        std::cout << "No argument selected.\n";
+    }
+    
 return 0;
 }
