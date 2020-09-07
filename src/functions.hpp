3dc712434f545dba5258051e5a124446acf8e9b2
davidus27 <davidisbest27@gmail.com>
2020-09-07 23:29:55 +0200

    removing extra namespace and adding new method definitions

--- src/functions.hpp
+++ src/functions.hpp
@@ -1,48 +1,48 @@
 #pragma once
 
 
-namespace frame
+enum class EthernetStandard
 {
-        
-    enum class EthernetStandard
-    {
-        EthernetII,
-        NovellRAW,
-        IEE,
-        IEE_LLC_SNAP
-    };
+    EthernetII,
+    NovellRAW,
+    IEE,
+    IEE_LLC_SNAP
+};
 
-    enum class Layer2
-    {
+enum class Layer2
+{
 
-    };
+};
 
-    enum class Layer3
-    {
+enum class Layer3
+{
 
-    };
+};
 
-    class ProcessedPacket
-    {
-    private:
-        const uint32_t packet_size_recv;
-        const uint32_t packet_size_real;
-        EthernetStandard eth_type;
-        const uint8_t* mac_dst;
-        const uint8_t* mac_src;
-        uint8_t* ip_dst;
-        uint8_t* ip_src;
-        Layer2 protocol;
-        Layer3 p2;
-        
-    public:
-        ProcessedPacket(const struct pcap_pkthdr* packet_header, const uint8_t* packet_body);
-        ~ProcessedPacket();
-    };
-}
+class ProcessedPacket
+{
+private:
+    const uint32_t packet_size_recv;
+    const uint32_t packet_size_real;
+    EthernetStandard eth_type;
+    const uint8_t* mac_dst;
+    const uint8_t* mac_src;
+    uint8_t* ip_dst;
+    uint8_t* ip_src;
+    Layer2 protocol;
+    Layer3 p2;
+    
+public:
+    ProcessedPacket(const struct pcap_pkthdr* packet_header, const uint8_t* packet_body);
+    ~ProcessedPacket();
+    void PrintSourceMAC();
+    void PrintDestinationMAC();
+    friend std::ostream& operator<<(std::ostream& os, const ProcessedPacket& packet);
+};
 
 void process_packet(
-    uint8_t* args,
+    std::vector<ProcessedPacket>& args,
     const struct pcap_pkthdr* packet_header,
     const uint8_t* packet_body
 );
+std::ostream& operator<<(std::ostream& os, const ProcessedPacket& packet);
