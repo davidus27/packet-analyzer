cd28ad26f28ab34e5c69437421fb1b85af4fcc3b
davidus27 <davidisbest27@gmail.com>
2020-09-07 23:32:55 +0200

    adding print operator overload for processed packet, function to print mac address and formatting packet payload for better readability

--- src/functions.cpp
+++ src/functions.cpp
@@ -3,46 +3,69 @@
 #include <pcap.h>
 #include <stdint.h>
 #include <memory>
+#include <vector>
 
 #include "constants.hpp"
 #include "functions.hpp"
 
-uint16_t convert(uint16_t value) { return (((value & 0xff)<<8) | ((value & 0xff00)>>8)); }
+uint16_t big_endian_to_small(uint16_t value) { return (((value & 0xff)<<8) | ((value & 0xff00)>>8)); }
 
-namespace frame
+ProcessedPacket::ProcessedPacket(
+    const struct pcap_pkthdr* packet_header, 
+    const uint8_t* packet_body
+)
+    :packet_size_recv{packet_header->len}, packet_size_real{packet_header->caplen}
 {
-    ProcessedPacket::ProcessedPacket(
-        const struct pcap_pkthdr* packet_header, 
-        const uint8_t* packet_body
-    )
-        :packet_size_recv{packet_header->len}, packet_size_real{packet_header->caplen}
-    {
 
-        this->mac_dst = packet_body;
-        this->mac_src = packet_body + Ethernet::MAC_SIZE;
-        uint16_t* ether_type = (uint16_t*) (packet_body + Ethernet::ETHER_TYPE_OFFSET); 
-        std::cout << "Value: " << convert(*ether_type)<< '\n';
-        if(convert(*ether_type) >= 2048)
-        {
-            this->eth_type = frame::EthernetStandard::EthernetII;
-        }
-        else
-        {
-            this->eth_type = frame::EthernetStandard::NovellRAW;
-        }
-    }
-
-    ProcessedPacket::~ProcessedPacket()
-    {
-    }
+    this->mac_dst = packet_body;
+    this->mac_src = packet_body + Ethernet::MAC_SIZE;
+    uint16_t ether_type = big_endian_to_small(*(uint16_t*)(packet_body + Ethernet::ETHER_TYPE_OFFSET)); 
+    this->eth_type = ether_type >= 0x0800 ? EthernetStandard::EthernetII : EthernetStandard::NovellRAW;
 
 }
 
+ProcessedPacket::~ProcessedPacket()
+{
+}
 
+void PrintMACAddress(std::ostream& os, const uint8_t* address)
+{
+    for(int i = 0; i < Ethernet::MAC_SIZE; i++)
+    {
+        os << std::setfill('0') << std::setw(2) << std::hex << (short) address[i] << ' ';
+    }
+    os << '\n';
+}
+
+std::ostream& operator<<(std::ostream& os, const ProcessedPacket& packet)
+{
+    os << std::dec << "dĺžka rámca poskytnutá pcap API – " << packet.packet_size_recv << " B \n"
+    << "dĺžka rámca prenášaného po médiu – " << packet.packet_size_real <<" B\n" 
+    << "Zdrojová MAC adresa: ";
+    PrintMACAddress(os, packet.mac_src);
+    os << "Cieľová MAC adresa: ";
+    PrintMACAddress(os, packet.mac_dst);
+    return os;
+}
+
+void print_packet(
+    std::ostream& os, 
+    const struct pcap_pkthdr* packet_header,
+    const uint8_t* packet_body
+)
+{
+    for(int i = 0; i < packet_header->len; i++)
+    {
+        os << std::setfill('0') << std::setw(2) << std::hex << (short) packet_body[i] << ' ';
+        if(!((i+1) % 16)) os << '\n'; 
+        else if(!((i+1) % 8)) os << ' '; 
+    }
+    os<<'\n';
+}
 
 
 void process_packet(
-    uint8_t* args,
+    std::vector<ProcessedPacket>& args,
     const struct pcap_pkthdr* packet_header,
     const uint8_t* packet_body
 )
@@ -53,14 +76,7 @@ void process_packet(
     }
     else {
         std::cout << "Packet body: \n";
-
-        frame::ProcessedPacket my_packet(packet_header, packet_body);
-        for(int i = 0; i < packet_header->len; i++)
-        {
-            
-            std::cout << std::setfill('0') << std::setw(2) << std::hex << (short) packet_body[i] << ' ';
-            if(!((i+1) % 10)) std::cout << '\n'; 
-        }
-        std::cout << '\n';
+        args.push_back(ProcessedPacket{packet_header, packet_body});
+        print_packet(std::cout, packet_header, packet_body);
     }
 }
