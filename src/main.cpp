6386efcf57852fa3976862fd5f60340b75c63617
davidus27 <davidisbest27@gmail.com>
2020-09-07 23:29:08 +0200

    calculating amount of packets and creating vector list of processed packets for printout

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <memory>
+#include <vector>
+
 #include <pcap.h>
 #include "functions.hpp"
 
+void increment_argument(int args[]) { args[0]++; }
+
+
 int main(int argc, char *argv[])
 {
-
     char errbuf[PCAP_ERRBUF_SIZE];
     const uint8_t* packet;
     struct pcap_pkthdr packet_header;
@@ -13,9 +18,26 @@ int main(int argc, char *argv[])
         pcap_t* handle = pcap_open_offline(argv[1], errbuf);
         if(handle)
         {
-            pcap_loop(handle, 0, process_packet, nullptr);
+            //std::vector<ProcessedPacket> *list = new std::vector<ProcessedPacket>();
+            std::unique_ptr<std::vector<ProcessedPacket>> list = std::make_unique<std::vector<ProcessedPacket>>();
+            int counter = 0;
+            pcap_loop(handle, 0, (pcap_handler)increment_argument, (u_char*)&counter);
+            handle = pcap_open_offline(argv[1], errbuf);
+            
+            list->reserve(counter);
+            pcap_loop(handle, 0, (pcap_handler)process_packet, (u_char*)list.get());
             pcap_close(handle);
-            std::cout<<"File: "<< argv[1]<<" closed\n";
+            std::cout<<"File: " << argv[1] <<" closed\n";
+
+            // ----------------- TEST ---------------------
+            int count = 0;
+            for(auto a = list->begin(); a != list->end(); a++)
+            {
+                std::cout << std::dec << "Ramec " << count++ << '\n';
+                std::cout << *a << '\n';
+            }
+            //delete[] list;
+
         }
         else
         {
@@ -27,5 +49,6 @@ int main(int argc, char *argv[])
         std::cout << "No argument selected.\n";
     }
     
+
 return 0;
 }
