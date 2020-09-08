39b0242de1406188c6afdb2f0547fff39fcf54c6
davidus27 <davidisbest27@gmail.com>
2020-09-08 22:16:30 +0200

    adding IP_SIZE magic value

--- src/constants.hpp
+++ src/constants.hpp
@@ -5,6 +5,7 @@ namespace Ethernet
     const uint8_t MAC_SIZE = 6; // bytes
     const uint8_t ETHER_TYPE = 2; // bytes
     const uint8_t ETHER_TYPE_OFFSET = 12; // bytes
+    const uint8_t IP_SIZE = 4; // bytes
     const uint8_t IPX = 3; // bytes
     const uint8_t FCS = 4; // bytes
 }
