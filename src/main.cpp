b892f26d6afe0b0c6ab2d88cbbf5228307350cb4
davidus27 <davidisbest27@gmail.com>
2020-09-09 23:45:40 +0200

    adding option to print the whole packet payload after printing info

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,20 @@
 
 void increment_argument(int args[]) { args[0]++; }
 
-void packet_printout(std::ostream& os, const std::unique_ptr<std::vector<ProcessedPacket>>& list)
+void packet_printout(std::ostream& os, char* argument, const std::unique_ptr<std::vector<ProcessedPacket>>& list)
 {
     int frame_count = 1;
+    pcap_t* handle = pcap_open_offline(argument, nullptr);
+    struct pcap_pkthdr packet_header;
     for(auto a = list->begin(); a != list->end(); a++)
     {
         os << std::dec << "Ramec " << frame_count++ << '\n';
-        os << *a << '\n';
+        os << *a;
+        
+        const uint8_t* packet = pcap_next(handle, &packet_header);
+        //print_packet(os, &packet_header, packet);
+        
+        os << '\n';
     }
 }
 
@@ -31,8 +38,8 @@ int main(int argc, char *argv[])
             
             std::unique_ptr<std::vector<ProcessedPacket>> list = std::make_unique<std::vector<ProcessedPacket>>();
             list->reserve(counter);
-            pcap_loop(handle, 5, (pcap_handler)process_packet, (uint8_t*)list.get());
-            packet_printout(std::cout, list);
+            pcap_loop(handle, 0, (pcap_handler)process_packet, (uint8_t*)list.get());
+            packet_printout(std::cout, argv[1], list);
 
             pcap_close(handle);
             std::cout<<"File: " << argv[1] <<" closed\n";            
