14ad8cf0f79d7185e28bca34446cb18941741ed4
davidus27 <davidisbest27@gmail.com>
2020-09-09 11:47:04 +0200

    refactoring for better readability

--- src/main.cpp
+++ src/main.cpp
@@ -7,32 +7,35 @@
 
 void increment_argument(int args[]) { args[0]++; }
 
+void packet_printout(std::ostream& os, const std::unique_ptr<std::vector<ProcessedPacket>>& list)
+{
+    int frame_count = 1;
+    for(auto a = list->begin(); a != list->end(); a++)
+    {
+        os << std::dec << "Ramec " << frame_count++ << '\n';
+        os << *a << '\n';
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    char errbuf[PCAP_ERRBUF_SIZE];
     if(argc > 1)
     {
+        char errbuf[PCAP_ERRBUF_SIZE];
         pcap_t* handle = pcap_open_offline(argv[1], errbuf);
         if(handle)
         {
-            std::unique_ptr<std::vector<ProcessedPacket>> list = std::make_unique<std::vector<ProcessedPacket>>();
             int counter = 0;
             pcap_loop(handle, 0, (pcap_handler)increment_argument, (uint8_t*)&counter);
             handle = pcap_open_offline(argv[1], errbuf);
             
+            std::unique_ptr<std::vector<ProcessedPacket>> list = std::make_unique<std::vector<ProcessedPacket>>();
             list->reserve(counter);
             pcap_loop(handle, 5, (pcap_handler)process_packet, (uint8_t*)list.get());
-            pcap_close(handle);
-            std::cout<<"File: " << argv[1] <<" closed\n";
+            packet_printout(std::cout, list);
 
-            // ----------------- PRINTOUT ---------------------
-            int frame_count = 1;
-            for(auto a = list->begin(); a != list->end(); a++)
-            {
-                std::cout << std::dec << "Ramec " << frame_count++ << '\n';
-                std::cout << *a << '\n';
-            }
+            pcap_close(handle);
+            std::cout<<"File: " << argv[1] <<" closed\n";            
         }
         else
         {
