f5df7e576dda9caa63b28e0e68c7108273b63bce
davidus27 <davidisbest27@gmail.com>
2020-09-09 23:44:30 +0200

    editing ethernet constants

--- src/constants.hpp
+++ src/constants.hpp
@@ -3,11 +3,11 @@
 namespace Ethernet
 {
     const uint8_t MAC_SIZE = 6; // bytes
-    const uint8_t ETHER_TYPE = 2; // bytes
-    const uint8_t ETHER_TYPE_OFFSET = 12; // bytes
     const uint8_t IP_SIZE = 4; // bytes
-    const uint8_t IPX = 3; // bytes
-    const uint8_t FCS = 4; // bytes
+    const uint8_t ETHER_TYPE_OFFSET = 12; // bytes
+    const uint8_t IPX_OFFSET = 12; // bytes
+    const uint8_t SAP_OFFSET = 12; // bytes
+    
 }
 
 namespace IP
