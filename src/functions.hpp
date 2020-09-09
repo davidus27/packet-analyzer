7d5249f07503d065c7d22ec0f76d572badeef6a8
davidus27 <davidisbest27@gmail.com>
2020-09-09 23:46:48 +0200

    adding small corrections in the header file

--- src/functions.hpp
+++ src/functions.hpp
@@ -5,9 +5,10 @@ enum class EthernetStandard
 {
     EthernetII,
     NovellRAW,
-    IEE,
-    IEE_LLC_SNAP
+    IEEE_LLC,
+    IEEE_LLC_SNAP
 };
+
 
 enum class Layer2
 {
@@ -21,28 +22,35 @@ enum class Layer3
 
 struct ProcessedPacket
 {
-private:
-    const uint32_t packet_size_recv;
-    const uint32_t packet_size_real;
-    std::vector<uint8_t> tcp_header;
-    uint8_t mac_dst[Ethernet::MAC_SIZE];
-    uint8_t mac_src[Ethernet::MAC_SIZE];
-    uint8_t ip_dst[Ethernet::IP_SIZE];
-    uint8_t ip_src[Ethernet::IP_SIZE];
-
-    EthernetStandard eth_type;
-    Layer2 protocol;
-    Layer3 p2;
-    
 public:
     ProcessedPacket(const struct pcap_pkthdr* packet_header, const uint8_t* packet_body);
     ~ProcessedPacket();
     friend std::ostream& operator<<(std::ostream& os, const ProcessedPacket& packet);
+
+private:
+    const uint32_t packet_size_recv;
+    const uint32_t packet_size_real;
+    uint8_t mac_dst[Ethernet::MAC_SIZE];
+    uint8_t mac_src[Ethernet::MAC_SIZE];
+    uint8_t ip_dst[Ethernet::IP_SIZE];
+    uint8_t ip_src[Ethernet::IP_SIZE];
+    EthernetStandard eth_type;
+    Layer2 protocol;
+    Layer3 p2;
+
+    const uint8_t* set_ethernet_type(const uint8_t* packet_body);
+
 };
 
 
 void PrintMACAddress(std::ostream& os, const uint8_t* address);
 void PrintIPAddress(std::ostream& os, const uint8_t* address);
+
+void print_packet(
+    std::ostream& os, 
+    const struct pcap_pkthdr* packet_header,
+    const uint8_t* packet_body
+);
 
 void process_packet(
     std::vector<ProcessedPacket>& args,
@@ -50,3 +58,4 @@ void process_packet(
     const uint8_t* packet_body
 );
 std::ostream& operator<<(std::ostream& os, const ProcessedPacket& packet);
+std::ostream& operator<<(std::ostream& os, const EthernetStandard& standard);
