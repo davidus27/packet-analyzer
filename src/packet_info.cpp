8c704c324b057dbd23210741637635948f0622a1
davidus27 <davidisbest27@gmail.com>
2020-09-27 22:46:36 +0200

    moving all helpful functions

--- src/packet_info.cpp
+++ src/packet_info.cpp
@@ -14,49 +14,6 @@ const char* configurations[] = {
     "configs/udp.config"
 };
 
-std::vector<std::pair<int, std::string>> load_configurations(const std::string& name)
-{
-    std::string text;
-    std::ifstream filename{name};
-    std::vector<std::pair<int, std::string>> pairs;
-    if(filename.is_open())
-    {
-        int position;
-        while(getline(filename, text))
-        {
-            position = text.find_first_of(' ');
-            pairs.push_back({
-                std::stoi(text.substr(0, position), nullptr, 16),
-                text.substr(position+1, text.size())});
-        }
-        filename.close();
-    }
-    else 
-    {
-        std::cout << "Unable to open file"; 
-        return std::vector<std::pair<int, std::string>>();
-    }
-    return pairs;
-}
-
-
-uint16_t big_endian_to_small(uint16_t value) { return (((value & 0xff)<<8) | ((value & 0xff00)>>8)); }
-
-uint16_t ProcessedInfo::get_ether_type(const uint8_t* packet_body)
-{
-    switch (this->ethernet_standard)
-    {
-        case EthernetStandard::EthernetII:
-            packet_body += Ethernet::ETHER_TYPE_II_OFFSET;
-            break;
-        case EthernetStandard::IEEE_LLC_SNAP:
-            packet_body += Ethernet::ETHER_TYPE_LLC_OFFSET;
-            break;
-        default:
-            return 0;
-    }
-    return big_endian_to_small(*(uint16_t*)(packet_body));
-}
 
 const uint8_t* ProcessedInfo::set_ethernet_type(const uint8_t* packet_body)
 {
@@ -86,6 +43,7 @@ const uint8_t* ProcessedInfo::set_ethernet_type(const uint8_t* packet_body)
     return data + 8; // 8 bytes: DSAP + SSAP + Control + Vendor + EtherType
 }
 
+
 ProcessedInfo::ProcessedInfo(const struct pcap_pkthdr* packet_header, const uint8_t* packet_body)
     :data{packet_header, packet_body}
 {
@@ -94,30 +52,21 @@ ProcessedInfo::ProcessedInfo(const struct pcap_pkthdr* packet_header, const uint
         this->mac_dst[i] = packet_body[i];
         this->mac_src[i] = packet_body[i+Ethernet::MAC_SIZE];
     }
+
     const uint8_t* data_start = this->set_ethernet_type(packet_body);
     if(data_start)
     {
-        // Getting IP
-        for(auto& conf_pair : load_configurations(configurations[0]))
-        {
-            if(conf_pair.first == get_ether_type(packet_body))
-            {
-                this->ether_type = conf_pair.second;
-            }
-        }
+        this->set_network_layer(packet_body, configurations[0]);
         // Getting TCP/UDP/ICMP
-        for(auto& conf_pair : load_configurations(configurations[1]))
-        {
-            if(conf_pair.first == data_start[9]) // 1 byte of EtherType + 9 bytes of IP Header
-            {
-                this->transport_protocol = conf_pair.second;
-            }
-        }
+        this->set_transport_layer(data_start, configurations[1]);
+
+        // Save IP addresses
         for(int i = 0; i < Ethernet::IP_SIZE; i++)
         {
             this->ip_src[i] = data_start[i+12];
             this->ip_dst[i] = data_start[i+16];
         }
+
         // Shift by size of IPv4 header from IHL value.
         // Size is in octets so multiply by 4
         uint8_t ihl_value = data_start[0] & 0xf;
@@ -125,36 +74,11 @@ ProcessedInfo::ProcessedInfo(const struct pcap_pkthdr* packet_header, const uint
 
         if(this->transport_protocol == "TCP")
         {
-            for(auto& conf_pair : load_configurations(configurations[2])) // Check TCP config
-            {
-                if(conf_pair.first == big_endian_to_small(*(uint16_t*)transport_data_start))
-                {
-                    this->src_port = conf_pair;
-                }
-                if(conf_pair.first == big_endian_to_small(*(uint16_t*)(transport_data_start+2)))
-                {
-                    this->dst_port = conf_pair;
-                }
-            }
-            if(!this->src_port.first) this->src_port.first = big_endian_to_small(*(uint16_t*)transport_data_start);
-            if(!this->dst_port.first) this->dst_port.first = big_endian_to_small(*(uint16_t*)(transport_data_start+2));
+            this->set_ports(transport_data_start, configurations[2]);
         }
-
         else if(this->transport_protocol == "UDP")
         {
-            for(auto& conf_pair : load_configurations(configurations[3])) // Check UDP config
-            {
-               if(conf_pair.first == big_endian_to_small(*(uint16_t*)transport_data_start))
-               {
-                   this->src_port = conf_pair;
-               }
-               if(conf_pair.first == big_endian_to_small(*(uint16_t*)(transport_data_start+2)))
-               {
-                   this->dst_port = conf_pair;
-               }
-            }
-            if(!this->src_port.first) this->src_port.first = big_endian_to_small(*(uint16_t*)transport_data_start);
-            if(!this->dst_port.first) this->dst_port.first = big_endian_to_small(*(uint16_t*)transport_data_start+2);
+            this->set_ports(transport_data_start, configurations[3]);    
         }
     }
     
