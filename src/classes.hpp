0af05547fc949e970406042d85663b25900522de
davidus27 <davidisbest27@gmail.com>
2020-09-27 22:00:34 +0200

    remove unused enum classes and move constants to classes.hpp

--- src/classes.hpp
+++ src/classes.hpp
@@ -1,5 +1,15 @@
 #pragma once
-#include "constants.hpp"
+
+namespace Ethernet
+{
+    const uint8_t MAC_SIZE = 6; // bytes
+    const uint8_t IP_SIZE = 4; // bytes
+    const uint8_t ETHER_TYPE_II_OFFSET = 12; // bytes
+    const uint8_t ETHER_TYPE_LLC_OFFSET = 18; // bytes
+    const uint8_t IPX_OFFSET = 12; // bytes
+    const uint8_t SAP_OFFSET = 12; // bytes
+    
+}
 
 enum class EthernetStandard
 {
@@ -7,17 +17,6 @@ enum class EthernetStandard
     NovellRAW,
     IEEE_LLC,
     IEEE_LLC_SNAP
-};
-
-
-enum class Layer2
-{
-
-};
-
-enum class Layer3
-{
-
 };
 
 struct Packet
@@ -35,6 +34,10 @@ public:
     ~ProcessedInfo();
     friend std::ostream& operator<<(std::ostream& os, const ProcessedInfo& packet);
 
+protected:
+    uint16_t get_ether_type(const uint8_t* packet_body);
+    const uint8_t* set_ethernet_type(const uint8_t* packet_body);
+
 private:
     uint8_t mac_dst[Ethernet::MAC_SIZE];
     uint8_t mac_src[Ethernet::MAC_SIZE];
@@ -42,10 +45,14 @@ private:
     uint8_t ip_src[Ethernet::IP_SIZE];
     EthernetStandard ethernet_standard;
     std::string ether_type;
+    std::string transport_protocol;
+    std::string  application_protocol;
+
+    std::pair<uint16_t, std::string> src_port; // value and name of source port
+    std::pair<uint16_t, std::string> dst_port; // value and name of destination port 
+    
     Packet data;
 
-    uint16_t get_ether_type(const uint8_t* packet_body);
-    const uint8_t* set_ethernet_type(const uint8_t* packet_body);
 };
 
 
