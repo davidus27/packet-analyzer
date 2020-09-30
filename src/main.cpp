fb7ce11bfad5879375773bc188d31ff2d5a00854
davidus27 <davidus27@gmail.com>
2020-09-30 23:37:42 +0200

    finding the most frequent ip address

--- src/main.cpp
+++ src/main.cpp
@@ -16,33 +16,46 @@ void process_packet(
 {
     if(packet_body)
         args.push_back(ProcessedInfo{packet_header, packet_body});
-        //args.push_back(ProcessedInfo());
-
     else
         std::cout << "No packets found\n";
 }
 
-typedef std::array<uint8_t, Ethernet::IP_SIZE> ip;
+typedef std::pair<std::array<uint8_t, Ethernet::IP_SIZE>, int> ip;
 
 std::vector<ip> get_unique_addresses(std::vector<ProcessedInfo> packets)
 {
     std::vector<ip> used;
-    used.push_back(packets[0].ip_src);
+    used.push_back({packets[0].ip_src, 1});
     bool in_use = false;
     for(auto& packet : packets)
     {
         for(unsigned long previous = 0; previous < used.size(); previous++)
         {
-            if(packet.ip_src == used[previous])
+            if(packet.ip_src == used[previous].first)
             {
+                used[previous].second++;
                 in_use = true;
                 break;
             }
         }
-        if(!in_use) used.push_back(packet.ip_src);
+        if(!in_use) used.push_back({packet.ip_src, 1});
         in_use = false;   
     }
     return used;
+}
+
+void print_ip_addresses(std::ostream& os, std::vector<ProcessedInfo> packets)
+{
+    os << "IP adresy vysielajucich uzlov:\n";
+    ip most_frequent;
+    for(auto& address : get_unique_addresses(packets))
+    {
+        os << address.first << '\n';
+        if(address.second > most_frequent.second) most_frequent = address;
+    }
+    os << "Adresa " << most_frequent.first 
+    << " ma najväčší počet odoslaných paketov: " 
+    << most_frequent.second << ".\n";
 }
 
 void inc(int& a){a++;}
@@ -60,17 +73,14 @@ int main(int argc, char *argv[])
             pcap_loop(handle, 0, (pcap_handler)process_packet, (uint8_t*)&packets);
             pcap_close(handle);
             
+    
             std::ofstream myfile;
             myfile.open("skuska.txt");
             myfile << packets;
-            //std::cout << packets << '\n';
-            
+
             // Another part of assignment
-            myfile << " IP adresy vysielajucich uzlov:\n";
-            for(auto& address : get_unique_addresses(packets))
-            {
-                myfile << address;
-            }
+            print_ip_addresses(myfile, packets);
+    
             myfile.close();
         }
         else
